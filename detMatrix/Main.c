be39cfd9085d9980e3306b035a7256d902fafbf9
will <WTS012201@gmail.com>
2021-02-16 11:15:00 -0600

    Determinant of a matrix

---
+++ detMatrix/Main.c
@@ -0,0 +1,22 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "matrix.h"
+int main(){
+	int i, j, n;
+	scanf("%d", &n);
+	float** matrix = (float**)malloc(n*sizeof(float*));
+	
+	for(i = 0; i < n; i++){
+		matrix[i] = (float*)malloc(n*sizeof(float));
+	}
+
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			float val;
+			scanf("%f", &val);
+			matrix[i][j] = val;
+		}
+	}
+	
+	printf("\ndetA = %.2f\n", det(matrix, n));
+}
