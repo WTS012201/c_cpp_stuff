03392cb56adfd31fa9458d614f367d19dbac879b
will <WTS012201@gmail.com>
2021-02-16 13:57:07 -0600

    renamed dir

--- detMatrix/matrix.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MATRIX_H
-#define __MATRIX_H
-
-float det(float **matrix, int n);
-float tr(float **matrix, int n);
-#endif
--- detMatrix/trMatrix.c
+++
@@ -1,9 +0,0 @@
-#include "stdlib.h"
-#include "matrix.h"
-
-float tr(float **matrix, int n){
-	n--;
-	if(n == 0)
-		return matrix[n][n];	
-	return matrix[n][n] + tr(matrix, n);
-}
